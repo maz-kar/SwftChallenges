f698c9b5ad3147088e969a206dd9f7df3faf46c1
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-03-25 17:57:54 +0100

    StringsView was added.

---
+++ SwiftChallenges/StringsView.swift
@@ -0,0 +1,18 @@
+//
+//  StringsView.swift
+//  SwiftChallenges
+//
+//  Created by Maziar Layeghkar on 25.03.24.
+//
+
+import SwiftUI
+
+struct StringsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StringsView()
+}
