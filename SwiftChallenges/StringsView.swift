e35c6d0c699cdcb29bba5bb34d1b00c70a83a12e
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-03-25 18:00:37 +0100

    areLettersUniqueA and B methods were added.

--- SwiftChallenges/StringsView.swift
+++ SwiftChallenges/StringsView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+class StringsViewModel: ObservableObject {
+    
+    func areLettersUniqueA(input: String) -> Bool { //AaBbCc -> true, Hello, world -> false
+        var usedLetters: [Character] = []
+        
+        for char in input {
+            if usedLetters.contains(String(char)) {
+                return false
+            }
+            usedLetters.append(char)
+        }
+        return true
+    }
+    
+    func areLettersUniqueB(input: String) -> Bool {
+        return input.count == Set(input).count
+    }
+    
+    
+}
+
 struct StringsView: View {
+    @StateObject private var vm = StringsViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Text("areLettersUniqueA: \(vm.areLettersUniqueA(input: "AaBbCc").description)")
+            
+            Text("areLettersUniqueB: \(vm.areLettersUniqueB(input: "Hello World!").description)")
+        }
     }
 }
 
