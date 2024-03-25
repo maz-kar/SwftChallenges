3db405f732db8b2065f2a189438d579724f385db
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-03-25 17:54:35 +0100

    Initial Commit

---
+++ SwiftChallenges/SwiftChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallengesApp.swift
+//  SwiftChallenges
+//
+//  Created by Maziar Layeghkar on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
