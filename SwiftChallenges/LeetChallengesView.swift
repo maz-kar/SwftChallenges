df172e6242b47c5d632863a20a9e08309b888940
maziar layeghkar <maziar.layeghkar@icloud.com>
2024-11-18 17:41:29 +0100

    filteredCountries func added.

--- SwiftChallenges/LeetChallengesView.swift
+++ SwiftChallenges/LeetChallengesView.swift
@@ -35,11 +35,19 @@ struct LeetChallengesView: View {
     private func tasks() -> [String] {
         return [
             "highestScore. Output: \(vm.highestScore(of: ["Alice": 85, "Bob": 92, "Charlie": 92, "Diana": 88]))",
+            "filteredCounties. Output: \(vm.filteredCounties(by: ["USA": 331, "India": 1393, "Germany": 83, "Japan": 126, "Mexico": 128], threshold: 300))"
+            
         ]
     }
 }
 
 class LeetChallengesViewModel: ObservableObject {
+    /*
+     Write a Swift function that takes a dictionary of students and their scores, where the key is a student's name (String) and the value is their score (Int). The function should:
+
+     Return the name of the student with the highest score.
+     If there are multiple students with the same highest score, return all their names as a list.
+     */
     func highestScore(of students: [String:Int]) -> [String] {
         var result: [String] = []
         guard let maxVal = students.values.max() else { return result}
@@ -50,6 +58,22 @@ class LeetChallengesViewModel: ObservableObject {
             }
         }
         return result
+    }
+    
+    
+    /*
+     Write a Swift function that takes a dictionary where the keys are country names (String) and the values are their populations (Int). The function should:
+
+     Filter the dictionary to include only countries with populations greater than 50 million.
+     Return a new dictionary containing the filtered results, sorted by population in descending order.
+     */
+    
+    func filteredCounties(by population: [String:Int], threshold: Int) -> [String:Int] {
+        return Dictionary(
+            uniqueKeysWithValues: population
+                .filter { $0.value > threshold }
+                .sorted { $0.value > $1.value }
+        )
     }
 }
 
